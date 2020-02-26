39193f0396260bb1b2a5ee781ed3336123a89b89
AliHaidry <30366779+AliHaidry@users.noreply.github.com>
2020-02-26 11:20:03 -0500

    updating through vs code.

--- project-two/cupertino_store/lib/model/product.dart
+++ project-two/cupertino_store/lib/model/product.dart
@@ -1,3 +1,5 @@
+// Changes made to the file through vs code - linking the remote/upstream concept.
+import 'package:flutter/foundation.dart';
 import 'package:flutter/foundation.dart';
 
 enum Category {
@@ -14,22 +16,21 @@ class Product {
     @required this.isFeatured,
     @required this.name,
     @required this.price,
-  })  : assert(category != null),
-        assert(id != null),
-        assert(isFeatured != null),
-        assert(name != null),
-        assert(price != null);
+  })  : assert(category != null, 'category must not be null'),
+        assert(id != null, 'id must not be null'),
+        assert(isFeatured != null, 'isFeatured must not be null'),
+        assert(name != null, 'name must not be null'),
+        assert(price != null, 'price must not be null');
 
-    final Category category;
-    final int id;
-    final bool isFeatured;
-    final String name;
-    final int price;
+  final Category category;
+  final int id;
+  final bool isFeatured;
+  final String name;
+  final int price;
 
-    String get assetName => 'id-0.jpg';
-    String get assetPackage => 'shrine_images';
+  String get assetName => '$id-0.jpg';
+  String get assetPackage => 'shrine_images';
 
-    @override 
-    String toString() => '$name (id=$id)';
-
+  @override
+  String toString() => '$name (id=$id)';
 }
